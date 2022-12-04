a1d7e8fce74ade38c1d9d28bb134d46eef1764bc
Héctor <hector.naranjo01@estudiant.upf.edu>
2022-12-04 16:25:33 +0100

    Change code myTeam

--- myTeam.py
+++ myTeam.py
@@ -1,5 +1,5 @@
-# baselineTeam.py
-# ---------------
+# myTeam.py
+# ---------
 # Licensing Information:  You are free to use or extend these projects for
 # educational purposes provided that (1) you do not distribute or publish
 # solutions, (2) you retain this notice, and (3) you provide clear
@@ -12,28 +12,17 @@
 # Pieter Abbeel (pabbeel@cs.berkeley.edu).
 
 
-# baselineTeam.py
-# ---------------
-# Licensing Information: Please do not distribute or publish solutions to this
-# project. You are free to use and extend these projects for educational
-# purposes. The Pacman AI projects were developed at UC Berkeley, primarily by
-# John DeNero (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
-# For more info, see http://inst.eecs.berkeley.edu/~cs188/sp09/pacman.html
-
-import random
-import contest.util as util
-
-from contest.captureAgents import CaptureAgent
-from contest.game import Directions
-from contest.util import nearestPoint
-
+import random, util
+from util import nearestPoint, manhattanDistance
+from captureAgents import CaptureAgent
+from game import Actions, Directions
 
 #################
 # Team creation #
 #################
 
 def create_team(first_index, second_index, is_red,
-                first='OffensiveReflexAgent', second='DefensiveReflexAgent', num_training=0):
+                first='KillerAgent', second='NonKillerAgent', num_training=0):
     """
     This function should return a list of two agents that will form the
     team, initialized using firstIndex and secondIndex as their agent
@@ -48,6 +37,8 @@ def create_team(first_index, second_index, is_red,
     any extra arguments, so you should make sure that the default
     behavior is what you want for the nightly contest.
     """
+
+    # The following line is an example only; feel free to change it.
     return [eval(first)(first_index), eval(second)(second_index)]
 
 
@@ -55,11 +46,7 @@ def create_team(first_index, second_index, is_red,
 # Agents #
 ##########
 
-class ReflexCaptureAgent(CaptureAgent):
-    """
-    A base class for reflex agents that choose score-maximizing actions
-    """
-
+class NonKillerAgent(CaptureAgent):
     def __init__(self, index, time_for_computing=.1):
         super().__init__(index, time_for_computing)
         self.start = None
@@ -83,9 +70,8 @@ class ReflexCaptureAgent(CaptureAgent):
         best_actions = [a for a, v in zip(actions, values) if v == max_value]
 
         food_left = len(self.get_food(game_state).as_list())
-
         if food_left <= 2:
-            best_dist = 9999
+            best_dist = 999
             best_action = None
             for action in actions:
                 successor = self.get_successor(game_state, action)
@@ -115,58 +101,8 @@ class ReflexCaptureAgent(CaptureAgent):
         Computes a linear combination of features and feature weights
         """
         features = self.get_features(game_state, action)
-        weights = self.get_weights(game_state, action)
+        weights = self.get_weights()
         return features * weights
-
-    def get_features(self, game_state, action):
-        """
-        Returns a counter of features for the state
-        """
-        features = util.Counter()
-        successor = self.get_successor(game_state, action)
-        features['successor_score'] = self.get_score(successor)
-        return features
-
-    def get_weights(self, game_state, action):
-        """
-        Normally, weights do not depend on the game state.  They can be either
-        a counter or a dictionary.
-        """
-        return {'successor_score': 1.0}
-
-
-class OffensiveReflexAgent(ReflexCaptureAgent):
-    """
-  A reflex agent that seeks food. This is an agent
-  we give you to get an idea of what an offensive agent might look like,
-  but it is by no means the best or only way to build an offensive agent.
-  """
-
-    def get_features(self, game_state, action):
-        features = util.Counter()
-        successor = self.get_successor(game_state, action)
-        food_list = self.get_food(successor).as_list()
-        features['successor_score'] = -len(food_list)  # self.getScore(successor)
-
-        # Compute distance to the nearest food
-
-        if len(food_list) > 0:  # This should always be True,  but better safe than sorry
-            my_pos = successor.get_agent_state(self.index).get_position()
-            min_distance = min([self.get_maze_distance(my_pos, food) for food in food_list])
-            features['distance_to_food'] = min_distance
-        return features
-
-    def get_weights(self, game_state, action):
-        return {'successor_score': 100, 'distance_to_food': -1}
-
-
-class DefensiveReflexAgent(ReflexCaptureAgent):
-    """
-    A reflex agent that keeps its side Pacman-free. Again,
-    this is to give you an idea of what a defensive agent
-    could be like.  It is not the best or only way to make
-    such an agent.
-    """
 
     def get_features(self, game_state, action):
         features = util.Counter()
@@ -174,12 +110,39 @@ class DefensiveReflexAgent(ReflexCaptureAgent):
 
         my_state = successor.get_agent_state(self.index)
         my_pos = my_state.get_position()
-
-        # Computes whether we're on defense (1) or offense (0)
-        features['on_defense'] = 1
-        if my_state.is_pacman: features['on_defense'] = 0
+        food_list = self.get_food(successor).as_list()
 
         # Computes distance to invaders we can see
+        enemies = [successor.get_agent_state(i) for i in self.get_opponents(successor)]
+        invaders = [a for a in enemies if a.is_pacman and a.get_position() is not None]
+        features['num_invaders'] = len(invaders)
+
+        # Don't get into a loop at the biginging and don't go through walls
+        if action == Directions.STOP: features['stop'] = 1
+        rev = Directions.REVERSE[game_state.get_agent_state(self.index).configuration.direction]
+        if action == rev: features['reverse'] = 1
+        
+        # By distance
+        if len(food_list) > 0:  # This should always be True,  but better safe than sorry
+            my_pos = successor.get_agent_state(self.index).get_position()
+            min_distance = min([self.get_maze_distance(my_pos, food) for food in food_list])
+            features['distance_to_food'] = min_distance
+        features['successor_score'] =  -len(food_list)
+        return features
+
+    def get_weights(self):
+        return {'num_invaders': -10000, 'stop': -100, 'reverse': -1, 'distance_to_food': -0.5, 'successor_score': 100}
+
+class KillerAgent(NonKillerAgent):
+    def get_features(self, game_state, action):
+        features = util.Counter()
+        successor = self.get_successor(game_state, action)
+
+        my_state = successor.get_agent_state(self.index)
+        my_pos = my_state.get_position()
+        food_list = self.get_food(successor).as_list()
+
+        # The killer has to look for invaders
         enemies = [successor.get_agent_state(i) for i in self.get_opponents(successor)]
         invaders = [a for a in enemies if a.is_pacman and a.get_position() is not None]
         features['num_invaders'] = len(invaders)
@@ -187,11 +150,27 @@ class DefensiveReflexAgent(ReflexCaptureAgent):
             dists = [self.get_maze_distance(my_pos, a.get_position()) for a in invaders]
             features['invader_distance'] = min(dists)
 
+        # Don't get into a loop at the biginging and don't go through walls
         if action == Directions.STOP: features['stop'] = 1
         rev = Directions.REVERSE[game_state.get_agent_state(self.index).configuration.direction]
         if action == rev: features['reverse'] = 1
 
+        # By distance
+        if len(food_list) > 0:  # This should always be True,  but better safe than sorry
+            my_pos = successor.get_agent_state(self.index).get_position()
+            min_distance = min([self.get_maze_distance(my_pos, food) for food in food_list])
+            features['distance_to_food'] = min_distance
+        features['successor_score'] =  -len(food_list)
+        # Get the capsules and go for them or camp them
+        features['capsule'] = 0
+        if len(game_state.get_capsules()) > 1:
+            if game_state.is_on_red_team(self.index):
+                capsule = game_state.get_capsules()[1]
+                features['capsule'] = self.get_maze_distance(my_pos, capsule)
+            else:
+                capsule = game_state.get_capsules()[0]
+                features['capsule'] = self.get_maze_distance(my_pos, capsule)
         return features
 
-    def get_weights(self, game_state, action):
-        return {'num_invaders': -1000, 'on_defense': 100, 'invader_distance': -10, 'stop': -100, 'reverse': -2}
+    def get_weights(self):
+        return {'num_invaders': -100, 'invader_distance': -10000, 'stop': -100, 'reverse': -1, 'distance_to_food': -0.5, 'successor_score': 100, 'capsule': -100}
